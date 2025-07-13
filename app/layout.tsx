dfcb2355a9f5a0800ae2eae18093027ba97ba145
Marcello Novelli <marcellonovelli@Marcellos-Mini.fritz.box>
2025-07-13 17:01:24 +0200

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import { Public_Sans } from "next/font/google";
 import type { ReactNode } from "react";
 import "./global.css";
+import Script from "next/script";
 
 const inter = Public_Sans({
   subsets: ["latin"],
@@ -14,8 +15,21 @@ export const metadata: Metadata = {
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const isDev = process.env.NODE_ENV === "development";
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={inter.className}
+      suppressHydrationWarning
+      data-website-id="72ec0e40-aae4-4ef8-9cd1-36b54a80ce47"
+    >
+      {!isDev ? (
+        <Script
+          async
+          src="https://cloud.umami.is/script.js"
+          data-website-id="72ec0e40-aae4-4ef8-9cd1-36b54a80ce47"
+        />
+      ) : null}
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
